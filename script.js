c7e88099c3b3a2123727b413f90defbf1183e25e
haku <haku.oknw@gmail.com>
2021-06-14 20:59:35 +0900

    ヘッダー(html)を追加

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // ハンバーガー
-$(function(){
-  $(".hamburger-btn,.header__navList a").on("click",function(){
-    $(".hamburger-btn,.header__navList").toggleClass("active")
+$(function() {
+  $('.hamburger').click(function() {
+      $(this).toggleClass('active');
+
+      if ($(this).hasClass('active')) {
+          $('.globalMenuSp').addClass('active');
+      } else {
+          $('.globalMenuSp').removeClass('active');
+      }
   });
 });
 
